e5aef65c253ec2ca447c5d0b22b05f4972c5e9db
LeonGamerPS1 <fnfmaker666@gmail.com>
2025-08-10 18:00:49 +0200

    charactericon rewrite to support icon changes midsong

--- source/game/objects/character/CharacterIcon.hx
+++ source/game/objects/character/CharacterIcon.hx
@@ -2,12 +2,26 @@ package game.objects.character;
 
 import flixel.FlxSprite;
 
-class CharacterIcon extends FlxSprite {
-    public function new(x:Float, y:Float, id:String) {
-        super(x, y);
-        loadGraphic(Paths.image('icon', 'data/characters/$id'), true, 150, 150);
-        animation.add('alive', [0], 1, true);
-        animation.add('dead', [1], 1, true);
-        animation.play('alive');
-    }
+class CharacterIcon extends FlxSprite
+{
+	public var lastIconID:String = ""; // useful if theres a script that does stuff
+
+	public function new(x:Float, y:Float, id:String)
+	{
+		super(x, y);
+		changeIcon(id);
+	}
+
+	public function changeIcon(newID:String = 'dad')
+	{
+		if (!Paths.exists(Paths.getPath('icon.png', 'data/characters/$newID'))) // checking if the icon exists or not
+			newID = 'dad'; // set icon to dad if it doesnt exist
+
+		loadGraphic(Paths.image('icon', 'data/characters/$newID'), false); // inital loadgraphic to get the sheet width and height
+
+		loadGraphic(Paths.image('icon', 'data/characters/$newID'), true, Math.floor(width / 2), Math.floor(height)); // width / 2 to split the sheet into two frames
+		animation.add('alive', [0], 1, true);
+		animation.add('dead', [1], 1, true);
+		animation.play('alive');
+	}
 }
